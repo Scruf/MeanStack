0a244a6787bddee4665b3ae5f25ad6fcf602d5ce
Egor <ekozitski@gmail.com>
2015-10-23 15:34:55 +0000

    Changed app file

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/api', api);
-//app.use('/auth', authenticate);
+app.use('/auth', authenticate);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
